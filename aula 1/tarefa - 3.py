ec45da4aae284605b790f166acdb9ca961ee3d9a
Marcos Acioly <marcosacioly43@gmail.com>
2025-04-01 21:29:48 -0300

    meu segundo commit

---
+++ aula 1/tarefa - 3.py
@@ -0,0 +1,27 @@
+from gettext import lngettext
+
+# Recebendo entrada do usuário
+print()
+n1 = float(input("Por favor, digite o primeiro número: "))
+
+print()
+
+n2 = float(input("Por favor, digite o segundo número: "))
+
+print()
+
+# Somando os números
+resultado = n1 + n2
+
+print()
+
+# Exibindo o resultado
+print("A soma do número", n1, " + ", n2, "é: ", resultado)
+
+print()
+
+
+
+
+
+
