4b7c124b42bac440c8186ee60d48343b2c070090
Marcos Acioly <marcosacioly43@gmail.com>
2025-04-02 21:20:51 -0300

    seven work

---
+++ aula 1/tarefa - 7.py
@@ -0,0 +1,16 @@
+# Solicitar o comprimento do lado do quadrado ao usuário
+print()
+lado = float(input("Digite o comprimento do lado do quadrado: "))
+print()
+
+# Calcular a área do quadrado usando a fórmula: área = lado²
+area = lado ** 2
+
+# Calcular o dobro da área
+dobro_area = area * 2
+
+# Exibir os resultados
+print(f"A área do quadrado é: {area:.2f}")
+print()
+print(f"O dobro da área do quadrado é: {dobro_area:.2f}")
+print()
