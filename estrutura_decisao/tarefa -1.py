735b18b2ba58321772a0cb4d49eb593021b66c5a
Marcos Acioly <marcosacioly43@gmail.com>
2025-04-04 21:27:46 -0300

    tarefa1

---
+++ estrutura_decisao/tarefa -1.py
@@ -0,0 +1,23 @@
+# Faça um algoritmo que peça dois números e imprimqa o maior deles
+
+# Solicita dois números ao usuário
+print()
+numero1 = float(input("Digite o primeiro número: "))
+print()
+numero2 = float(input("Digite o segundo número: "))
+
+print()
+
+# Verifica qual é o maior número ou se são iguais
+if numero1 > numero2:
+    print(f"O maior número é: {numero1}")
+elif numero2 > numero1:
+    print(f"O maior número é: {numero2}")
+else:
+    print("Os números são iguais.")
+
+
+
+
+
+
--- estruturaderepeticao/tarefa - 1.py
+++
@@ -1,9 +0,0 @@
-
-nota = 8.0
-
-if nota > 7.0:
-    print('Aprovado!!!')
-elif nota < 3:
-    print("reprovado!!!")
-else:
-    print("Recuperação!!!")
