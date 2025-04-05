165b9fe811250d5536f10fc7e5204c55496a7628
Marcos Acioly <marcosacioly43@gmail.com>
2025-04-04 21:23:40 -0300

    tarefa 1 de estrutura de repetição

---
+++ estruturaderepeticao/tarefa - 2.py
@@ -0,0 +1,15 @@
+# EXERCÍCIO 02
+
+
+idade_str = input ('Digite sua idade:  ')
+idade = int(idade_str)
+
+if (idade > 18)
+    print('Você é maior de idade.')
+
+else:
+if (idade > 12):
+    print('Você é maior de idade.')
+
+elif (idade > 12:
+print('Você é maior de idade.')
