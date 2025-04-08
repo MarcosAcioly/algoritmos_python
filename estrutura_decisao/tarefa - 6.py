58697ccf7f8d1d5d49aea4db352d0636d199b805
Marcos Acioly <marcosacioly43@gmail.com>
2025-04-07 21:50:37 -0300

    aula dia 07-04

---
+++ estrutura_decisao/tarefa - 6.py
@@ -0,0 +1,32 @@
+# Faça um algoritmo que leia três números e mostre o maior deles.
+
+
+# Solicita dois números ao usuário
+print()
+numero1 = float(input("Digite o primeiro número: "))
+print()
+numero2 = float(input("Digite o segundo número: "))
+print()
+numero3 = flat(input('Digite o terceiro número:  '))
+
+print()
+
+# Verifica qual é o maior número ou se são iguais
+if numero1 > numero2 and numero 2 > numero3:
+    print(f"O maior número é: {numero1}")
+
+    if numero2 > numero1 and numero3 < numero2:
+        print(f"O maior número é: {numero2}")
+
+elif n
+
+
+if numero1 > numero2:
+    print(f"O maior número é: {numero1}")
+elif numero2 > numero1:
+    print(f"O maior número é: {numero2}")
+
+
+
+else:
+    print("Os números são iguais.")
